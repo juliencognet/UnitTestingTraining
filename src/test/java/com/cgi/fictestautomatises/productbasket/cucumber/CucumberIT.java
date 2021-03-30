a1e930032d886f444d09e98355dc8256db4b0879
dev <jean.neyret@cgi.com>
2021-03-30 13:04:15 +0000

    Removed ignore on Cucumber IT

--- src/test/java/com/cgi/fictestautomatises/productbasket/cucumber/CucumberIT.java
+++ src/test/java/com/cgi/fictestautomatises/productbasket/cucumber/CucumberIT.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
-@Ignore
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/features")
 public class CucumberIT  {
